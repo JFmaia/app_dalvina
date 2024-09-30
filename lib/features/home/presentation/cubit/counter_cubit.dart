25aa829d52c600847b8e252d3a0f10cc69c1c584
JFmaia <jfmaia.dev@gmail.com>
2024-09-30 18:30:35 -0300

    feat: Deleted folders and modified locales

--- lib/features/home/presentation/cubit/counter_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:app_dalvina/features/home/presentation/cubit/counter_state.dart';
-import 'package:bloc/bloc.dart';
-
-class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeStateInitial());
-
-  void increment() => emit(HomeStateIncremented(state.count + 1));
-}
